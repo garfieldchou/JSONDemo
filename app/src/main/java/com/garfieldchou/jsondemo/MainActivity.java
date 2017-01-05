20fa8ea62d974e27fa530bd3d9ff21cb374c33f0
shaudai <shaudai0402@hotmail.com>
2017-01-06 07:39:55 +0800

    Create a JSONObject to extract the weather info
    JSONObject
    .getString()

--- app/src/main/java/com/garfieldchou/jsondemo/MainActivity.java
+++ app/src/main/java/com/garfieldchou/jsondemo/MainActivity.java
@@ -1,13 +1,78 @@
 package com.garfieldchou.jsondemo;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadJSONTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            URL url;
+            HttpURLConnection urlconnection = null;
+            String result = "";
+
+            try {
+                url = new URL(urls[0]);
+                urlconnection = (HttpURLConnection) url.openConnection();
+                InputStream in = urlconnection.getInputStream();
+                InputStreamReader reader = new InputStreamReader(in);
+
+                int data = reader.read();
+
+                while (-1 != data) {
+                    char current = (char) data;
+                    result += current;
+                    data = reader.read();
+                }
+                return result;
+            }
+            catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            catch (IOException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            super.onPostExecute(result);
+
+            try {
+                JSONObject jsonObject = new JSONObject(result);
+                String weatherInfo = jsonObject.getString("weather");
+
+                Log.i("Weather content", weatherInfo);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadJSONTask task = new DownloadJSONTask();
+        task.execute("http://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=9c436ed4e80da1d03390836a27581377");
+
     }
 }
